735b09307303aaf4849fbbde0e3a9f87692e445b
Skif <skifnsh@gmail.com>
2026-09-03 21:08:50 +0300

    protocol: add compact P0 detection summary encoder

--- firmware/include/zs_protocol.h
+++ firmware/include/zs_protocol.h
@@ -1,8 +1,12 @@
 #ifndef ZS_PROTOCOL_H
 #define ZS_PROTOCOL_H
+
 #include "zs_types.h"
 #include <stddef.h>
 #include <stdint.h>
+
 size_t zs_protocol_encode_detection(const zs_detection_t *m, uint8_t *out, size_t cap);
+size_t zs_protocol_encode_detection_summary(const zs_detection_t *m, uint8_t *out, size_t cap);
 uint16_t zs_float_to_f16(float f);
+
 #endif
