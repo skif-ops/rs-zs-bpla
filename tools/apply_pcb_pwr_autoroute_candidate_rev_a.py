52f37195ad9576a34e5dfa7d48b523a5b0f0289c
Skif <skifnsh@gmail.com>
2026-09-25 08:58:21 +0300

    pcb-pwr: autoroute 011 run 13 - ground vias under U1/U2 bodies, NT3 ground link

--- tools/apply_pcb_pwr_autoroute_candidate_rev_a.py
+++ tools/apply_pcb_pwr_autoroute_candidate_rev_a.py
@@ -60,7 +60,8 @@ PASSES = 150
 # - fan-outs of the LMR60440 pin rows 5..8 (U3/U4) and of U2 with vias, FB/MODE
 #   on B.Cu to R1/R5/R10/R9, EN_MODEM corridor, NT2 net-tie exits, DNP I2C
 #   pull-ups R13/R14 and test point TP9 (TP10 is left to the routers), 3V3 links C7->R13/R14 and
-#   R15/U2->C5.
+#   R15/U2->C5; GND_PWR of U1 pin 2 and U2 pins 1/2 to vias under the package
+#   bodies, NT3 GND_PWR pad to the NT2 ground via.
 PREROUTE = [
     ('track', 'F.Cu', '3V3_DIGITAL', 0.2, [(46.2, 24.0), (47.7, 24.0)]),
     ('via', None, '3V3_DIGITAL', 0.6, [(47.7, 24.0)]),
@@ -152,6 +153,11 @@ PREROUTE = [
     ('via', None, '3V3_DIGITAL', 0.6, [(72.0, 40.1)]),
     ('via', None, '3V3_DIGITAL', 0.6, [(73.0, 40.1)]),
     ('track', 'B.Cu', '3V3_DIGITAL', 1.2, [(72.0, 40.1), (76.5, 38.0), (82.95, 38.0), (82.95, 44.5), (81.08, 46.0)]),
+    ('track', 'F.Cu', 'GND_PWR', 0.3, [(18.7, 30.0), (20.0, 30.0)]),
+    ('via', None, 'GND_PWR', 0.6, [(20.0, 30.0)]),
+    ('track', 'F.Cu', 'GND_PWR', 0.3, [(42.3, 22.0), (42.8, 21.5), (44.4, 21.5)]),
+    ('via', None, 'GND_PWR', 0.6, [(44.4, 21.5)]),
+    ('track', 'F.Cu', 'GND_PWR', 0.3, [(76.6, 48.52), (76.6, 51.5), (76.1, 52.0)]),
 ]
 TIE_STRIPS = ["NT2"]
 # Mounting holes H1..H4 (native board) for the gap-fill router keepouts, and the
